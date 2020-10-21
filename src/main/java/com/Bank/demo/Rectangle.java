3a06d380f04ed99d372c08208373c83f9d13f8f8
rabin <098sonamla@gmail.com>
2020-10-21 13:43:24 -0500

    Intial commit ::basic configure of the shape structure

---
+++ src/main/java/com/Bank/demo/Rectangle.java
@@ -0,0 +1,13 @@
+package com.Bank.demo;
+
+public class Rectangle {
+
+    public int rectangle(){
+        int l=5;
+        int b=4;
+        int h=2;
+        int a= l*b*h;
+
+        return a;
+    }
+}
---
+++ src/main/java/com/Bank/demo/shape.java
@@ -0,0 +1,12 @@
+package com.Bank.demo;
+
+public class shape {
+    public double circleShape() {
+
+        double   r=0.5;
+        final double v = r * r;
+        return v;
+
+    }
+
+}
